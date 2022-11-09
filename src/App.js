278b7d128868a1d3c0bc1bddc9237385b11bc566
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-11-09 10:29:58 -0500

    CS-FE:FEAT 1 Create project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './assets/css/App.css';
+import Logo from './assets/images/logoMG.png';
+
+const App = () => {
+	return (
+		<div className='App'>
+			<img src={Logo} alt={Logo} />
+			<h1>Base Project</h1>
+		</div>
+	);
+};
+
+export default App;

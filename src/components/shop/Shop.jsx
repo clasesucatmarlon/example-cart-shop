e630425fe28b8e04b066df58c4f0a1b40cdfad0d
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-11-10 09:23:28 -0500

    CS-FE: FEAT 2 Created product section of the shop

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { PropTypes } from "prop-types";
+import Products from '../products/Products';
 
-const Shop = () => {
-	return <div>Shop</div>;
+const Shop = ({allProducts}) => {
+	return (
+		<div>
+			<h1>Store</h1>
+			<Products
+				allProducts={allProducts}	
+			/>
+		</div>
+	);
+};
+
+Shop.propTypes  = {
+	allProducts: PropTypes.array.isRequired
 };
 
 export default Shop;

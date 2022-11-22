8d12a823116140555db1460ed79c3f2dcd2b4dcc
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-11-22 09:43:10 -0500

    CS-FE: FEAT 9 Created logical to add to cart

--- src/reducers/shopReducer.js
+++ src/reducers/shopReducer.js
@@ -9,10 +9,45 @@ import stateInitial from '../data/dataProducts';
 const reducer = (state = stateInitial, action) => {
 	switch (action.type) {
 		case 'ADD_PRODUCT_TO_CART':
-			// console.log('se ejecuta la función en reducer');
-			// console.log('id', action.idProdAdd);
-			// console.log('name', action.nameProdAdd);
-			return state;
+			// console.log(state.cart)
+			if (state.cart.length === 0) {
+				return {
+					...state,
+					cart: [
+						{ id: action.idProdAdd, name: action.nameProdAdd, quantity: 1 },
+					],
+				};
+			} else {
+				// Clonar listCart
+				const newListCart = [...state.cart];
+				// Comprobar si el cart ya tiene el id
+				const foundProdInCart =
+					newListCart.filter((item) => item.id === action.idProdAdd).length > 0;
+
+				if (foundProdInCart) {
+					// Si ya está el id se actualiza el cart en base a la posición
+					newListCart.forEach((prodCart, index) => {
+						if (prodCart.id === action.idProdAdd) {
+							const quantityProd = newListCart[index].quantity;
+							newListCart[index] = {
+								id: action.idProdAdd,
+								name: action.nameProdAdd,
+								quantity: quantityProd + 1,
+							};
+						}
+					});
+				} else {
+					newListCart.push({
+						id: action.idProdAdd,
+						name: action.nameProdAdd,
+						quantity: 1,
+					});
+				}
+				return {
+					...state,
+					cart: newListCart,
+				};
+			}
 		default:
 			return state;
 	}
